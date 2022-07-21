f67dc1b46401bf8bc108437b6ca22c1989ee5c75
Sandra <sandra.villot038@gmail.com>
2022-07-21 16:12:52 +0200

    formulaire lost ok

--- src/app/lost/lost.page.ts
+++ src/app/lost/lost.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../api/user.service';
 
 @Component({
   selector: 'app-lost',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lost.page.scss'],
 })
 export class LostPage implements OnInit {
-
-  constructor() { }
+  status: boolean;
+  description: string;
+  date: Date;
+  location: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  constructor(public apiService: UserService) { 
+  }
 
   ngOnInit() {
   }
+  submitForm() {
+    let data = {
+      status: 0,
+      description: this.description,
+      date: this.date,
+      location: this.location,
+      firstname: this.firstname,
+      lastname: this.lastname,
+      email: this.email
+    }
+    console.log(data);
+    
+    this.apiService.submitForm(data).subscribe((res) => {
+      console.log("SUCCES ===", res);
+  })
+}
 
 }
+
