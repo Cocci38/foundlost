c4e5f0d12e463ea4e6b6e5903fb0e93d69bfb19a
Sandra <sandra.villot038@gmail.com>
2022-08-11 16:20:50 +0200

    insertion objet id_user + commentaire

--- src/app/lost/lost.page.ts
+++ src/app/lost/lost.page.ts
@@ -20,25 +20,31 @@ export class LostPage implements OnInit {
   firstname: string;
   lastname: string;
   email: string;
+  users_id: number;
   isSubmitted = false;
   constructor(public apiService: UserService, public formBuilder: FormBuilder, public toastController: ToastController, private route: ActivatedRoute, private router: Router) {
+
+    // Si il n'y a pas une session utilisateur on est renvoyé sur la page de connexion sinon on peut rester et on stocke les paramètre de session pour s'en servir plus loin
     if (!sessionStorage.getItem('username')) {
       this.router.navigateByUrl("/sign-up");
     } else {
       this.username = sessionStorage.getItem('username');
-      console.log(sessionStorage.getItem('username'));
+      // this.id_user = sessionStorage.getItem('id_user');
+      // console.log(sessionStorage.getItem('id_user'));
     }
   }
 
   ngOnInit() {
+    // Pattern pour la sécurisation du formulaire
     this.lostForm = this.formBuilder.group({
       status: ['0'],
-      description: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9-\' æœçéàèùâêîôûëïüÿÂÊÎÔÛÄËÏÖÜÀÆÇÉÈŒÙ]{3,}$')]],
+      description: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9-\' æœçéàèùâêîôûëïüÿÂÊÎÔÛÄËÏÖÜÀÆÇÉÈŒÙ]{10,}$')]],
       date: [null, Validators.required],
       location: ['', [Validators.required, Validators.pattern('^[a-zA-Z-\' æœçéàèùâêîôûëïüÿÂÊÎÔÛÄËÏÖÜÀÆÇÉÈŒÙ]{3,}$')]],
       firstname: ['', [Validators.required, Validators.pattern('^[a-zA-Z-\' æœçéàèùâêîôûëïüÿÂÊÎÔÛÄËÏÖÜÀÆÇÉÈŒÙ]{3,}$')]],
       lastname: ['', [Validators.required, Validators.pattern('^[a-zA-Z-\' æœçéàèùâêîôûëïüÿÂÊÎÔÛÄËÏÖÜÀÆÇÉÈŒÙ]{3,}$')]],
-      email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+      email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      users_id: [sessionStorage.getItem('id_user')]
     });
   }
   // getDate(e: { target: { value: string | number | Date; }; }) {
@@ -47,63 +53,82 @@ export class LostPage implements OnInit {
   //     onlyself: true
   //   })
   // }
+
+  // Pour afficher les erreurs :
   get errorControl() {
     return this.lostForm.controls;
   }
 
+  // Les Toast : 
+  // Si le formulaire est valide
   async valide() {
-    if (this.lostForm.valid) {
-      let toast = await this.toastController.create({
-        message: "Demande envoyée",
-        duration: 3000,
-        color: "success",
-        position: "middle"
-      })
-      toast.present();
-    } else {
-      let toast = await this.toastController.create({
-        message: "Demande non envoyée",
-        duration: 3000,
-        color: "danger",
-        position: "middle"
-      })
-      toast.present();
-    }
+    let toast = await this.toastController.create({
+      message: "Formulaire envoyée",
+      duration: 3000,
+      color: "success",
+      position: "bottom"
+    })
+    toast.present();
   }
-    // Toast de déconnexion réussi
-    async account() {
-      let toast = await this.toastController.create({
-        message: "Vous êtes déconnecté",
-        duration: 3000,
-        color: "success",
-        position: "bottom"
-      })
-      toast.present();
-    }
-  
-    // Fonction de déconnexion
-    disconnect() {
-      // On supprime la session (username et user_email)
-      sessionStorage.removeItem('username');
-      sessionStorage.removeItem('user_email');
-      sessionStorage.removeItem('id_user');
-      // On redirige vers la page de connexion
-      this.router.navigateByUrl("/sign-up");
-      // On lance le toat de déconnexion réussi
-      this.account();
-    }
+  // Si le formulaire est invalide
+  async invalid() {
+    let toast = await this.toastController.create({
+      message: "Formulaire non envoyée",
+      duration: 3000,
+      color: "danger",
+      position: "bottom"
+    })
+    toast.present();
+  }
+  // Si la déconnexion réussi
+  async account() {
+    let toast = await this.toastController.create({
+      message: "Vous êtes déconnecté",
+      duration: 3000,
+      color: "success",
+      position: "bottom"
+    })
+    toast.present();
+  }
+
+  // Fonction de déconnexion
+  disconnect() {
+    // On supprime la session (username, user_email et id_user)
+    sessionStorage.removeItem('username');
+    sessionStorage.removeItem('user_email');
+    sessionStorage.removeItem('id_user');
+    // On redirige vers la page de connexion
+    this.router.navigateByUrl("/sign-up");
+    // On lance le toat de déconnexion réussi
+    this.account();
+  }
+
+  // Validation du formulaire pour déclarer un objet perdu :
   submitForm() {
+    // On passe isSubmitted à true (pour afficher les erreurs une fois le formulaire soumis)
     this.isSubmitted = true;
+    // Si le formulaire n'est pas valide, on envoie le toast invalid et on retourne faux
     if (!this.lostForm.valid) {
-      console.log('Please provide all the required values!');
+      console.log('Le formulaire n\'est pas valide');
+      this.invalid();
       return false;
     } else {
       console.log(this.lostForm.value)
+      // Sinon le formulaire est valide et on envoie le formulaire vers le serveur
       this.apiService.submitForm(this.lostForm.value).subscribe((res) => {
         console.log("SUCCES ===", res);
-      })
+        // Si la réponse est égale à true, on lance le toats valid
+        if (res == true) {
+          this.valide();
+        } else {
+          // Sinon on envoie le toat invalid
+          this.invalid();
+        }
+      });
+      // On repasse isSubmitted à false
       this.isSubmitted = false;
     }
+    // On vide le formulaire
     this.lostForm.reset();
   }
 
